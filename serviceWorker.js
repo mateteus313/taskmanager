4779c2a36e4a206abd3a3efb4c26ca964aac04fd
Matheus Santos <mateteus07042000@gmail.com>
2024-07-17 11:32:54 -0300

    service-worker sync function  and push notification to advert about task expire date

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,63 @@
-// const staticTaskManager = "task-manager-v1";
-// const assets = ["/", "/index.html", "/css/style.css", "/js/app.js"];
+self.addEventListener('install', event => {
+  console.log('Service Worker installed.');
+  self.skipWaiting();
+});
 
-// self.addEventListener("install", (installEvent) => {
-//   installEvent.waitUntil(
-//     caches.open(staticTaskManager).then((cache) => {
-//       cache.addAll(assets);
-//     })
-//   );
-// });
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated.');
 
-// self.addEventListener("fetch", (fetchEvent) => {
-//   fetchEvent.respondWith(
-//     caches.match(fetchEvent.request).then((res) => {
-//       return res || fetch(fetchEvent.request);
-//     })
-//   );
-// });
+  return self.clients.claim();
+});
+
+self.addEventListener('sync', function(event) {
+  event.waitUntil(
+    syncTasksAndNotify()
+  );
+});
+
+async function syncTasksAndNotify() {
+  const tasks = await getTasksFromIndexedDB();
+  
+  tasks.forEach(task => {
+    let currentDate = new Date();
+    let taskExpirationDate = new Date(task.expirationDate);
+
+    if (currentDate.getDate() == taskExpirationDate.getDate()) {
+      self.registration.showNotification('Tarefa Pendente', {
+        body: `A tarefa: ${task.title} irá expirar amanhã!`,
+        icon: 'images/icons/icon32x32.png'
+      });
+    }
+    if (currentDate.getDate() > taskExpirationDate.getDate()) {
+      self.registration.showNotification('Tarefa Pendente', {
+        body: `A tarefa: ${task.title} irá expirar hoje!`,
+        icon: 'images/icons/icon32x32.png'
+      });
+    }
+  });
+}
+
+async function getTasksFromIndexedDB() {
+  return new Promise((resolve, reject) => {
+    const openRequest = indexedDB.open('task-manager', 1);
+
+    openRequest.onsuccess = event => {
+      const db = event.target.result;
+      const transaction = db.transaction('tasks', 'readonly');
+      const store = transaction.objectStore('tasks');
+      const getAllRequest = store.getAll();
+
+      getAllRequest.onsuccess = () => {
+        resolve(getAllRequest.result);
+      };
+
+      getAllRequest.onerror = () => {
+        reject(getAllRequest.error);
+      };
+    };
+
+    openRequest.onerror = () => {
+      reject(openRequest.error);
+    };
+  });
+}
